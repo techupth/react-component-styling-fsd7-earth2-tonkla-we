578ea08afa3c32fab1004802a0ec08f1a51ee891
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-22 14:44:29 +0700

    feat:we

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,118 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import alertTriangle from "../assets/alert-triangle.png";
+import alertCircle from "../assets/alert-circle.png";
 
-function Alert() {}
+import frown from "../assets/frown.png";
+import checkCircle from "../assets/check-circle.png";
+
+function Alert() {
+  const textStyle = "padding: 5px 0 5px 20px;";
+  const imageSyle = "  padding: 16px 0 5px 20px; height: 40px;";
+  return (
+    <>
+      <div
+        htmlFor=""
+        css={css`
+          width: 650px;
+          background-color: #f9c8c8;
+          display: flex;
+          height: 76px;
+          margin-bottom: 20px;
+        `}
+      >
+        <img
+          src={frown}
+          alt=""
+          css={css`
+            ${imageSyle}
+          `}
+        />
+        <h4
+          css={css`
+            ${textStyle}
+          `}
+        >
+          This is error alert box
+        </h4>
+      </div>
+      <div
+        htmlFor=""
+        css={css`
+          width: 650px;
+          background-color: #f9d9c8;
+          display: flex;
+          height: 76px;
+          margin-bottom: 20px;
+        `}
+      >
+        <img
+          src={alertTriangle}
+          alt=""
+          css={css`
+            ${imageSyle}
+          `}
+        />
+        <h4
+          css={css`
+            ${textStyle}
+          `}
+        >
+          This is warning alert box
+        </h4>
+      </div>
+      <div
+        htmlFor=""
+        css={css`
+          width: 650px;
+          background-color: #f9ebc8;
+          display: flex;
+          height: 76px;
+          margin-bottom: 20px;
+        `}
+      >
+        <img
+          src={alertCircle}
+          alt=""
+          css={css`
+            ${imageSyle}
+          `}
+        />
+        <h4
+          css={css`
+            ${textStyle}
+          `}
+        >
+          This is info alert box
+        </h4>
+      </div>
+      <div
+        htmlFor=""
+        css={css`
+          width: 650px;
+          background-color: #cef7cd;
+          display: flex;
+          height: 76px;
+          margin-bottom: 20px;
+        `}
+      >
+        <img
+          src={checkCircle}
+          alt=""
+          css={css`
+            ${imageSyle}
+          `}
+        />
+        <h4
+          css={css`
+            ${textStyle}
+          `}
+        >
+          This is success alert box
+        </h4>
+      </div>
+    </>
+  );
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,56 @@
-// Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+function Button() {
+  return (
+    <>
+      <div
+        css={css`
+          margin-bottom: 20px;
+        `}
+      >
+        <label
+          htmlFor=""
+          css={css`
+            margin-right: 100px;
+          `}
+        >
+          Primary
+        </label>
+        <button
+          css={css`
+            background-color: #074ee8;
+            border-radius: 4px;
+            color: white;
+            width: 140px;
+            height: 50px;
+          `}
+        >
+          Large
+        </button>
+      </div>
+      <div>
+        <label
+          htmlFor=""
+          css={css`
+            margin-right: 80px;
+          `}
+        >
+          Secondary
+        </label>
+        <button
+          css={css`
+            background-color: #07a4e8;
+            border-radius: 4px;
+            color: white;
+            width: 140px;
+            height: 50px;
+          `}
+        >
+          Large
+        </button>
+      </div>
+    </>
+  );
+}
 
-function Button() {}
+export default Button;
