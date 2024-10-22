55b6cbf03eedb9557798d69bf653912292a2db2b
Tonkla <Apisit378819@gmail.com>
2024-10-22 15:39:56 +0700

    feat:add

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,43 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button({ type }) {
+  return (
+    <>
+      <div>
+        {type === "primary" ? (
+          <button
+            css={css`
+              color: white;
+              background-color: #074ee8;
+              border-radius: 4px;
+              padding: 0 16px;
+              margin: 5px;
+              width: 172px;
+              height: 50px;
+            `}
+          >
+            Button
+          </button>
+        ) : (
+          <button
+            css={css`
+              color: white;
+              background-color: #07a4e8;
+              border-radius: 4px;
+              padding: 0 16px;
+              margin: 5px;
+              width: 172px;
+              height: 50px;
+            `}
+          >
+            Button
+          </button>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Button;
